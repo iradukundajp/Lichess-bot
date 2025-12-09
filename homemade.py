e8b99a38fa9609431511eae2c91b5940a471a6d0
Jean Paul IRADUKUNDA <ikundajp@gmail.com>
2025-12-09 01:14:49 +0100

    hosting

--- homemade.py
+++ homemade.py
@@ -9,7 +9,7 @@ import random
 from lib.engine_wrapper import MinimalEngine
 from lib.lichess_types import MOVE, HOMEMADE_ARGS_TYPE
 import logging
-
+from engines.bot.main import get_move
 
 # Use this logger variable to print messages to the console or log files.
 # logger.info("message") will always print "message" to the console or log file.
@@ -20,7 +20,13 @@ logger = logging.getLogger(__name__)
 class ExampleEngine(MinimalEngine):
     """An example engine that all homemade engines inherit."""
 
+class PyBot(ExampleEngine):
+    def search(self, board: chess.Board, time_limit: Limit, ponder: bool, draw_offered: bool, root_moves: MOVE) -> PlayResult:
+        print("GETTING MOVE!")
+        
+        move = get_move(board, 4)
 
+        return PlayResult(move, None)
 # Bot names and ideas from tom7's excellent eloWorld video
 
 class RandomMove(ExampleEngine):
